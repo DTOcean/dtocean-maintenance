da91241b14c98b5afa4dee4ea29414eb15d8dd4e
Mathew Topper <damm_horse@yahoo.co.uk>
2018-10-11 17:56:01 +0100

    Fix copyright notices and author strings.

--- dtocean_maintenance/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""This module contains the init functions of dtocean-operations-and-maintenance (currently empty)
-
-.. module:: init
-   :platform: Windows
-   :synopsis: init function computational code for dtocean-operations-and-maintenance
-   
-.. moduleauthor:: Bahram Panahandeh <bahram.panahandeh@iwes.fraunhofer.de>
-"""
